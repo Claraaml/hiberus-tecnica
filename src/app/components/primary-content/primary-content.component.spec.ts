4027eac43c710630bccd685ce3d3515b3363cce5
claraaml <claraisabelemele@gmail.com>
2021-11-03 20:34:49 +0100

    elimino todos los .spec para los test unitarios

--- src/app/components/primary-content/primary-content.component.spec.ts
+++ src/app/components/primary-content/primary-content.component.spec.ts
@@ -1,25 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PrimaryContentComponent } from './primary-content.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SpinnerService } from 'src/app/services/spinner.service';
+import { TranslateService } from '@ngx-translate/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
-describe('PrimaryContentComponent', () => {
-  let component: PrimaryContentComponent;
-  let fixture: ComponentFixture<PrimaryContentComponent>;
+describe('Primary Content Component', () => {
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PrimaryContentComponent ]
-    })
-    .compileComponents();
-  });
+    let component: PrimaryContentComponent;
+    let fixture: ComponentFixture<PrimaryContentComponent>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PrimaryContentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule
+            ],
+            declarations: [
+                PrimaryContentComponent
+            ],
+            providers: [
+                SpinnerService,
+                TranslateService
+            ],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+        });
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PrimaryContentComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
 });
